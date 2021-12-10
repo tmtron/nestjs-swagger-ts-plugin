cca2fde3a36473b48636fd7d3849ebcfae025f98
TmTron <martin.trummer@tmtron.com>
2021-12-10 12:00:55 +0100

    added @js-temporal/polyfill

--- src/cats/dto/create-cat.dto.ts
+++ src/cats/dto/create-cat.dto.ts
@@ -1,4 +1,7 @@
 import { IsInt, IsString } from 'class-validator';
+import {Temporal} from "@js-temporal/polyfill";
+import Instant = Temporal.Instant;
+import {Expose} from "class-transformer";
 
 export class CreateCatDto {
   @IsString()
@@ -9,4 +12,7 @@ export class CreateCatDto {
 
   @IsString()
   readonly breed: string;
+
+  @Expose({ name: 'date_time' })
+  readonly dateTime: Instant;
 }
